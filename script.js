35f019f5f86b31f8ff015d65b8595035544cf986
admin <admin@iMac3.local>
2018-10-23 17:09:58 +0200

    add style

--- script.js
+++ script.js
@@ -14,16 +14,22 @@ function getAjax(url, param) {
     let results = JSON.parse(xhttp.response);
     let list = results.meals;
     for (let i = 0; i < list.length; i++) {
+
         //debugger;
         let element = document.createElement("P");
         let div = document.createElement("DIV");    
+        let img = document.createElement("IMG");
 
         var main = document.querySelector(".result");
         //console.log();
+        div.classList.add('card');
         element.classList.add('el');
         element.dataset.id = list[i].idMeal;
         element.innerHTML = list[i][param];
+        img.src = list[i].strMealThumb;
+        img.classList.add('thumb');
         div.appendChild(element);
+        div.appendChild(img);
         main.appendChild(div); 
         //debugger;
     }
@@ -39,28 +45,32 @@ function getAjaxById(id) {
 
     for (let i = 0; i < results.length; i++) {
         //debugger;
-
+        
         let div = document.createElement("DIV");
         div.classList.add("popup");
+        
 
         let name = document.createElement("H3");
         let categ = document.createElement("P");
         let area = document.createElement("P");
         let instructions = document.createElement("P");
+        let leaveCross = document.createElement('IMG');
 
+        leaveCross.classList.add('leave');
+        leaveCross.src = "./img/64x64.png";
         name.innerHTML = results[0].strMeal;
         categ.innerHTML = results[0].strCategory;
         area.innerHTML = results[0].strArea;
         instructions.innerHTML = results[0].strInstructions;
 
-       
+        div.appendChild(leaveCross);
         div.appendChild(name);
         div.appendChild(categ);
         div.appendChild(area);
         div.appendChild(instructions);
         main.appendChild(div); 
 
-        /* TODO -> créer Markup html d'un popup */
+        
         console.log(results[0].strArea);
         console.log(results[0].strCategory);
     }
@@ -87,20 +97,21 @@ document.querySelector('body').addEventListener('keydown', function(el) {
 // END SEARCH
 
 // BUTTON TO SORT
-getCategories.addEventListener('click', function() {
-    content.innerHTML = "";
-    getAjax("https://www.themealdb.com/api/json/v1/1/list.php?c=list", "strCategory");
-});
 
-getStates.addEventListener('click', function() {
-    content.innerHTML = "";
-    getAjax("https://www.themealdb.com/api/json/v1/1/list.php?a=list", "strArea");
-});
+// getCategories.addEventListener('click', function() {
+//     content.innerHTML = "";
+//     getAjax("https://www.themealdb.com/api/json/v1/1/list.php?c=list", "strCategory");
+// });
 
-getIngredients.addEventListener('click', function() {
-    content.innerHTML = "";
-    getAjax("https://www.themealdb.com/api/json/v1/1/list.php?i=list", "strIngredient");
-});
+// getStates.addEventListener('click', function() {
+//     content.innerHTML = "";
+//     getAjax("https://www.themealdb.com/api/json/v1/1/list.php?a=list", "strArea");
+// });
+
+// getIngredients.addEventListener('click', function() {
+//     content.innerHTML = "";
+//     getAjax("https://www.themealdb.com/api/json/v1/1/list.php?i=list", "strIngredient");
+// });
 // END BUTTON TO SORT
 
 //BUTTON TOGGLE POPUP
@@ -109,9 +120,13 @@ document.querySelector('body').addEventListener('click', function(el) {
 
     
     if(el.target.classList[0] === "el" ) {
-   
         getAjaxById(el.target.dataset.id);
+
+    } if(el.target.classList[0] === "leave" ) {
+        el.target.parentElement.remove();
     }
+
+
 });
 // END BUTTON TOGGLE POPUP
 
